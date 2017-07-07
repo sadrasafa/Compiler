2ad3801c8b52fb2383dfc7399f9592ad0b59eafa
sadra <sadrasafa@gmail.com>
2017-07-07 21:45:21 +0430

    parser panic mode

--- src/Parser.java
+++ src/Parser.java
@@ -39,6 +39,50 @@ public class Parser {
             System.out.println("STACK: "+stack + "TOKEN: "+token.getName());
             top = stack.peek();
             action = parseTable.get(top).get(token);
+            if (action==null) {
+                //error recovery
+                boolean hasGoto = false;
+                while (true) {
+                    for (Symbol nt: grammar.getNonTerminals()) {
+                        if (parseTable.get(top).get(nt)!=null) {
+                            hasGoto = true;
+                        }
+                    }
+                    if (hasGoto) {
+                        break;
+                    }
+                    stack.pop();
+                    top = stack.peek();
+
+                } //top is set
+                boolean followSunda = false;
+                ArrayList<Symbol> NTlar = new ArrayList<>();
+                for (Symbol sym: grammar.getNonTerminals()) {
+                    if (parseTable.get(top).get(sym)!=null)
+                        NTlar.add(sym);
+                }
+                Symbol istadighimNT = new Symbol("dibaYetishdim");
+                while (!followSunda && !token.equals(Grammar.symDollar)) {
+//                    lastTkn = tkn; //TODO lastTokeni neyniyim?!!!
+                    tkn = scanner.getToken();
+                    token = tkn.getSymbol();
+                    //todo last Tokeni neyinim?
+                    for (Symbol sym : NTlar) {
+                        if (sym.getFollow().contains(token)) {
+                            followSunda = true;
+                            istadighimNT = sym;
+                        }
+                    }
+                } // NT is set or reached end of code
+                if (istadighimNT.getName().equals("dibaYetishdim")) {
+                    System.out.println("PANIC MODE REACHED END OF CODE");
+                    break;
+                }
+                action =  parseTable.get(top).get(istadighimNT);
+                Integer t = action.getDest();
+                stack.push(t);
+
+            }
             if (action.getType() == ParseAction.SHIFT) {
                 Integer t = action.getDest();
                 stack.push(t);
@@ -75,6 +119,7 @@ public class Parser {
             }
             else  {
                 //todo error recovery
+                System.out.println("BURA GARAH GIRMIYA USULAN :))");
             }
         }
 
